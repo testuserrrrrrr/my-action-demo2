a56b85bad0662d2389f7cb6918405ac4b7867bdf
testuserrrrrrr <crushonai69@proton.me>
2025-08-21 02:59:07 +0400

    Update main.go

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ import (
 
 func main() {
 	filename := flag.String("file", "./README.md", "target file")
+	owner := flag.String("owner", "John Doe", "user")
 	flag.Parse()
 	fmt.Printf("=========== %s ===========", *filename)
 	now := time.Now().String()
-	if err := os.WriteFile(*filename, []byte("\n"+now+"\n"), 0644); err != nil {
+	if err := os.WriteFile(*filename, []byte(*owner+"  \n"+now+"\n"), 0644); err != nil {
 		log.Fatal(err)
 	}
 }
