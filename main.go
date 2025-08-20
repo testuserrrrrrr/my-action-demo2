b3443881e6901b4fff3799fa32fabc80dfdebe0d
testuserrrrrrr <crushonai69@proton.me>
2025-08-21 02:06:33 +0400

    Update main.go

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"log"
 	"os"
 	"time"
+	"flag"
 )
 
-const filename = "README.md"
+//const filename = "README.md"
 
 func main() {
+	filename := flag.String("file", "./README.md", "target file")
 	now := time.Now().String()
-	if err := os.WriteFile(filename, []byte("\n"+now+"\n"), 0644); err != nil {
+	if err := os.WriteFile(*filename, []byte("\n"+now+"\n"), 0644); err != nil {
 		log.Fatal(err)
 	}
 }
